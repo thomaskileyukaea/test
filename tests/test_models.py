26204f4ab0c99e9ec4c0146d98509d4e7ee86b57
Thomas Kiley <138868636+thomaskileyukaea@users.noreply.github.com>
2023-08-08 14:31:18 +0100

    Initialize feature-std-dev

---
+++ tests/test_models.py
@@ -0,0 +1,48 @@
+"""Tests for statistics functions within the Model layer."""
+
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+def test_daily_mean_zeros():
+    """Test that mean function works for an array of zeros."""
+    from inflammation.models import daily_mean
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0, 0])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_mean(test_input), test_result)
+
+
+def test_daily_mean_integers():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_mean
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([3, 4])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_mean(test_input), test_result)
+
+@pytest.mark.parametrize('data, expected_standard_deviation', [
+    ([0, 0, 0], 0.0),
+    ([1.0, 1.0, 1.0], 0),
+    ([0.0, 2.0], 1.0)
+])
+def test_daily_standard_deviation(data, expected_standard_deviation):
+    from inflammation.models import s_dev
+    result_data = s_dev(data)['standard deviation']
+    npt.assert_approx_equal(result_data, expected_standard_deviation)
+
+
+def test_daily_standard_deviation_on_np_array():
+    from inflammation.models import s_dev
+    data = np.array([[0.0]])
+    expected_std_devs = [[0.0]]
+    out = s_dev(data)['standard deviation']
+    npt.assert_array_almost_equal(data, expected_std_devs)
